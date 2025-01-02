4b6c6d89c9ca8eb5ab4fda71a643ae0a090e0b5b
uuta <nightrage.metal66@gmail.com>
2025-01-02 14:04:46 +0900

    Update font size to 14 and add keybinds for tab navigation. Add pyright and ruff LSP configurations. Include black and isort formatting tools for PHP files

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -2,7 +2,7 @@ local lsp_servers = {
     "html",
     "lua_ls",
     "ts_ls",
-    "pyright",
+    -- "pyright",
 }
 
 return {
@@ -75,6 +75,41 @@ return {
                     client.server_capabilities.documentRangeFormattingProvider = false
                     client.server_capabilities.semanticTokensProvider = false
                 end,
+            })
+            require("lspconfig").pyright.setup({
+                settings = {
+                    pyright = {
+                        -- Using Ruff's import organizer
+                        disableOrganizeImports = true,
+                    },
+                    python = {
+                        analysis = {
+                            -- Ignore all files for analysis to exclusively use Ruff for linting
+                            ignore = { "*" },
+                        },
+                    },
+                },
+            })
+            require("lspconfig").ruff.setup({
+                init_options = {
+                    preferences = {
+                        importModuleSpecifierEnding = "minimal",
+                        importModuleSpecifierPreference = "non-relative",
+                        includeCompletionsForImportStatements = true,
+                        includeCompletionsForModuleExports = true,
+                    },
+                },
+                on_attach = function(client, bufnr)
+                    -- 必要に応じて on_attach 関数を追加
+                    print("Pyright attached to buffer " .. bufnr)
+                end,
+                handlers = {
+                    ["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+                        virtual_text = true,
+                        signs = true,
+                        underline = true,
+                    }),
+                },
             })
             vim.opt.completeopt = "menu,menuone,noselect"
         end,
