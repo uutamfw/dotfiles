ab7ac2e0ca7fec910e8ae7d0851994c9e06580bc
uuta <nightrage.metal66@gmail.com>
2025-03-04 00:28:57 +0900

    Update trouble.nvim plugin configuration options

--- .config/nvim/lua/plugins/trouble.lua
+++ .config/nvim/lua/plugins/trouble.lua
@@ -2,13 +2,56 @@
 --- https://github.com/folke/trouble.nvim
 return {
     "folke/trouble.nvim",
-    dependencies = "nvim-tree/nvim-web-devicons",
-    config = function()
-        require("trouble").setup({
-            -- your configuration comes here
-            auto_open = true,
-            auto_close = true,
-            auto_preview = true,
-        })
-    end,
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {
+        -- your configuration comes here
+        auto_open = true,
+        auto_close = true,
+        auto_preview = true,
+        -- 必須フィールドを追加
+        debug = false,
+        auto_refresh = true,
+        auto_jump = false,
+        focus = false,
+        restore = true,
+        follow = true,
+        indent_guides = true,
+        max_items = 200,
+        multiline = true,
+        pinned = false,
+        warn_no_results = true,
+        open_no_results = false,
+        win = {},
+        preview = {
+            type = "main",
+            scratch = true,
+        },
+        throttle = {
+            refresh = 20,
+            update = 10,
+            render = 10,
+            follow = 100,
+            preview = { ms = 100, debounce = true },
+        },
+        keys = {
+            ["?"] = "help",
+            r = "refresh",
+            R = "toggle_refresh",
+            q = "close",
+            o = "jump_close",
+            ["<esc>"] = "cancel",
+            ["<cr>"] = "jump",
+        },
+        modes = {},
+        icons = {
+            indent = {
+                top = "│ ",
+                middle = "├╴",
+                last = "└╴",
+                fold_open = " ",
+                fold_closed = " ",
+                ws = "  ",
+            },
+        },
+    },
 }
