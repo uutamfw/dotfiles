b6dde3ef1c9d757fe69d1eb0c2f9442d19c8c74f
uuta <nightrage.metal66@gmail.com>
2025-04-25 16:21:41 +0900

    Add Mason and Ruff LSP servers, configure LSP settings

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -1,97 +1,30 @@
-local log = require("core.log")
-local util = require("lspconfig.util")
-
 local lsp_servers = {
     "html",
     "lua_ls",
     "ts_ls",
     "pyright",
-    "pylsp",
-    "python-lsp-server",
+    "ruff_lsp",
 }
 
-local function is_installed(bin)
-    local result = vim.fn.executable(bin) == 1
-    log.dlog("[DEBUG] Checking executable: " .. bin .. " -> " .. tostring(result))
-    return result
-end
-
-is_installed("pylsp")
-is_installed("python-lsp-server")
-
-local function get_python_path(workspace)
-    if vim.env.VIRTUAL_ENV then
-        return vim.env.VIRTUAL_ENV .. "/bin/python"
-    end
-    -- workspace内に.venvまたはvenvがあるかチェック
-    for _, pattern in ipairs({ ".venv", "venv" }) do
-        local venv_path = util.path.join(workspace, pattern)
-        if util.path.exists(venv_path) then
-            return venv_path .. "/bin/python"
-        end
-    end
-    -- なければシステムのpythonをフォールバック
-    return "python"
-end
-
 return {
-    "neovim/nvim-lspconfig",
-    "williamboman/mason.nvim",
     {
-        -- Mason
-        "williamboman/mason-lspconfig.nvim",
+        "williamboman/mason.nvim",
+        build = ":MasonUpdate",
         config = function()
             require("mason").setup()
-            require("mason-lspconfig").setup_handlers({
-                function(server)
-                    require("lspconfig")[server].setup({
-                        -- Let mason-lspconfig install the defined servers
-                        ensure_installed = {
-                            lsp_servers,
-                        },
-                        -- Language server
-                        init_options = {
-                            preferences = {
-                                importModuleSpecifierEnding = "minimal",
-                                importModuleSpecifierPreference = "non-relative",
-                                includeCompletionsForImportStatements = true,
-                                includeCompletionsForModuleExports = true,
-                            },
-                        },
-                        on_attach = function(client)
-                            -- Disable formatting
-                            client.server_capabilities.documentFormattingProvider = false
-                            client.server_capabilities.documentRangeFormattingProvider = false
-                        end,
-                        handlers = {
-                            -- TODO: diagnostic configuration
-                            -- https://neovim.io/doc/user/lsp.html#lsp-api
-                            -- https://chat.deepseek.com/a/chat/s/eb212534-64c8-4907-afe5-4d22a51543a3
-                            ["textDocument/publishDiagnostics"] = vim.lsp.with(
-                                vim.lsp.diagnostic.on_publish_diagnostics,
-                                {
-                                    -- Disable virtual_text
-                                    virtual_text = true,
-                                    signs = true,
-                                    update_in_insert = true,
-                                    underline = true,
-                                    open = false,
-                                }
-                            ),
-                        },
-                    })
-                end,
-            })
-            require("lspconfig").dartls.setup({
-                cmd = { "dart", "language-server", "--protocol=lsp" },
-            })
-            require("lspconfig").gopls.setup({
-                cmd = { "gopls", "-rpc.trace", "--debug=localhost:6060" },
-                settings = {
-                    gopls = {
-                        experimentalWorkspaceModule = true,
-                    },
-                },
+        end,
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        dependencies = {
+            "williamboman/mason.nvim",
+            "neovim/nvim-lspconfig",
+        },
+        config = function()
+            -- Mason-lspconfigの設定
+            require("mason-lspconfig").setup({
+                automatic_installation = true,
+                ensure_installed = lsp_servers,
                 init_options = {
                     preferences = {
                         importModuleSpecifierEnding = "minimal",
@@ -100,94 +33,48 @@ return {
                         includeCompletionsForModuleExports = true,
                     },
                 },
-                on_attach = function(client, bufnr)
-                    client.server_capabilities.documentFormattingProvider = false
-                    client.server_capabilities.documentRangeFormattingProvider = false
-                    client.server_capabilities.semanticTokensProvider = false
-                end,
             })
-            require("lspconfig").pylsp.setup({
-                on_new_config = function(new_config, workspace)
-                    log.dlog("[DEBUG] workspace" .. tostring(workspace))
-                    log.dlog("[DEBUG] get_python_path" .. tostring(get_python_path(workspace)))
-                    -- new_config.cmd = {
-                    --     "/Users/yutaaoki/gg-newsletter-for-biz-packages/packages/core-api/.venv/bin/python",
-                    --     "-m",
-                    --     "pylsp",
-                    -- }
-                end,
-                on_attach = function(client)
-                    -- Disable formatting
-                    client.server_capabilities.documentFormattingProvider = false
-                    client.server_capabilities.documentRangeFormattingProvider = false
-                end,
-                root_dir = util.root_pattern(".git", ".venv", "setup.py", "pyproject.toml"),
-                init_options = {
-                    preferences = {
-                        importModuleSpecifierEnding = "minimal",
-                        importModuleSpecifierPreference = "non-relative",
-                        includeCompletionsForImportStatements = true,
-                        includeCompletionsForModuleExports = true,
-                    },
-                },
-                handlers = {
-                    ["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-                        virtual_text = true,
-                        signs = true,
-                        update_in_insert = true,
-                        underline = true,
-                        open = false,
-                    }),
-                },
+
+            -- 補完の設定
+            vim.cmd([[set completeopt+=menuone,noselect,popup]])
+
+            -- LSPサーバーの設定
+            vim.lsp.config("lua_ls", {
                 settings = {
-                    pylsp = {
-                        plugins = {
-                            -- ruff = { enabled = true },
-                            black = { enabled = false },
-                            autopep8 = { enabled = false },
-                            mccabe = { enabled = false },
-                            yapf = { enabled = false },
-                            pycodestyle = { enabled = false },
-                            pylint = { enabled = false },
-                            pyflakes = { enabled = false },
-                            pylsp_mypy = {
-                                enabled = true,
-                                live_mode = false,
-                                -- overrides = {
-                                --     "--python-executable",
-                                --     "/Users/yutaaoki/gg-newsletter-for-biz-packages/packages/core-api/.venv/bin/python",
-                                --     "--config-file",
-                                --     "/Users/yutaaoki/gg-newsletter-for-biz-packages/packages/core-api/mypy.ini",
-                                --     vim.fn.getcwd(),
-                                -- },
+                    Lua = {
+                        diagnostics = {
+                            globals = { "vim", "lua" },
+                        },
+                        format = {
+                            enable = true,
+                            defaultConfig = {
+                                indent_style = "space",
+                                indent_size = "2",
                             },
                         },
-                        -- disableOrganizeImports = true,
-                        -- disableTaggedHints = true,
+                    },
+                },
+            })
+
+            vim.lsp.config("ruff_lsp", {
+                -- pyproject.tomlの設定が自動的に読み込まれる
+            })
+
+            vim.lsp.config("pyright", {
+                settings = {
+                    pyright = {
+                        disableOrganizeImports = false,
                     },
                     python = {
                         analysis = {
-                            autoSearchPaths = true,
-                            diagnosticMode = "workspace",
-                            typeCheckingMode = "standard",
-                            useLibraryCodeForTypes = true,
-                            -- we can this setting below to redefine some diagnostics
-                            diagnosticSeverityOverrides = {
-                                deprecateTypingAliases = true,
-                            },
-                            -- inlay hint settings are provided by pylance?
-                            inlayHints = {
-                                callArgumentNames = "partial",
-                                functionReturnTypes = true,
-                                pytestParameters = true,
-                                variableTypes = true,
-                            },
+                            ignore = { "*" }, -- Ruffにlintを任せる場合
                         },
                     },
                 },
             })
-            vim.opt.completeopt = "menu,menuone,noselect"
+
+            -- LSPサーバーの有効化
+            vim.lsp.enable(lsp_servers)
         end,
     },
-    build = ":MasonUpdate",
 }
--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -4,70 +4,63 @@
 -- jose-elias-alvarez/null-ls.nvim
 -- https://github.com/nvimtools/none-ls.nvim
 local M = {
-    "nvimtools/none-ls.nvim",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        "vim-test/vim-test",
-    },
+	"nvimtools/none-ls.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"vim-test/vim-test",
+	},
 }
 
 function M.config()
-    local null_ls = require("null-ls")
-    local sources = {
-        -- Deleted rustfmt
-        -- Make sure the proper configurations from here
-        -- https://github.com/nvimtools/none-ls.nvim/issues/58
-        -- null_ls.builtins.formatting.rustfmt.with({filetypes = {"rust"}}),
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.dart_format,
-        null_ls.builtins.diagnostics.mypy.with({
-            extra_args = function()
-                -- local virtual = "/Users/yutaaoki/gg-newsletter-for-biz-packages/packages/core-api/.venv/bin"
-                -- local virtual = os.getenv("VIRTUAL_ENV") or os.getenv("CONDA_PREFIX") or "/usr"
-                -- return { "--python-executable", virtual .. "/bin/python" }
-            end,
-        }),
-        null_ls.builtins.formatting.gofmt,
-        null_ls.builtins.formatting.goimports,
-        null_ls.builtins.formatting.prettierd.with({
-            filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
-        }),
-        null_ls.builtins.diagnostics.phpmd.with({ filetypes = { "php" } }),
-    }
-    -- if you want to set up formatting on save, you can use this as a callback
-    local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-    -- Formatter
-    -- https://github.com/jose-elias-alvarez/null-ls.nvim
-    -- https://zenn.dev/nazo6/articles/c2f16b07798bab
-    -- Maybe this way? -> https://github.com/jose-elias-alvarez/null-ls.nvim/issues/844
-    local lsp_formatting = function()
-        vim.lsp.buf.format({
-            filter = function(client)
-                -- apply whatever logic you want (in this example, we'll only use null-ls)
-                -- return client.name == "null-ls"
-                return true
-            end,
-            async = false,
-        })
-    end
+	local null_ls = require("null-ls")
+	local sources = {
+		-- Deleted rustfmt
+		-- Make sure the proper configurations from here
+		-- https://github.com/nvimtools/none-ls.nvim/issues/58
+		-- null_ls.builtins.formatting.rustfmt.with({filetypes = {"rust"}}),
+		-- null_ls.builtins.formatting.stylua,
+		-- null_ls.builtins.formatting.dart_format,
+		-- null_ls.builtins.formatting.gofmt,
+		-- null_ls.builtins.formatting.goimports,
+		-- null_ls.builtins.formatting.prettierd.with({
+		--     filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
+		-- }),
+		-- null_ls.builtins.diagnostics.phpmd.with({ filetypes = { "php" } }),
+	}
+	-- if you want to set up formatting on save, you can use this as a callback
+	local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+	-- Formatter
+	-- https://github.com/jose-elias-alvarez/null-ls.nvim
+	-- https://zenn.dev/nazo6/articles/c2f16b07798bab
+	-- Maybe this way? -> https://github.com/jose-elias-alvarez/null-ls.nvim/issues/844
+	local lsp_formatting = function()
+		vim.lsp.buf.format({
+			filter = function(client)
+				-- apply whatever logic you want (in this example, we'll only use null-ls)
+				-- return client.name == "null-ls"
+				return true
+			end,
+			async = false,
+		})
+	end
 
-    null_ls.setup({
-        filetypes = {},
-        sources = sources,
-        on_attach = function(client, bufnr)
-            if client.supports_method("textDocument/formatting") then
-                vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-                vim.api.nvim_create_autocmd("BufWritePre", {
-                    group = augroup,
-                    buffer = bufnr,
-                    callback = function()
-                        lsp_formatting(bufnr)
-                    end,
-                })
-            end
-        end,
-        debug = false,
-    })
+	null_ls.setup({
+		filetypes = {},
+		sources = sources,
+		on_attach = function(client, bufnr)
+			if client.supports_method("textDocument/formatting") then
+				vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+				vim.api.nvim_create_autocmd("BufWritePre", {
+					group = augroup,
+					buffer = bufnr,
+					callback = function()
+						lsp_formatting(bufnr)
+					end,
+				})
+			end
+		end,
+		debug = false,
+	})
 end
 
 return M
