9d57126a2b5b28de1bfa1ef2f028ca78f6f6c02e
uuta <nightrage.metal66@gmail.com>
2025-05-17 18:50:57 +0900

    Add language formatters and format on save settings

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -1,10 +1,17 @@
 return {
-    "stevearc/conform.nvim",
-    config = function()
-        require("conform").setup({
-            formatters_by_ft = {
-                python = { "ruff" },
-            },
-        })
-    end,
+  "stevearc/conform.nvim",
+  config = function()
+    require("conform").setup({
+      formatters_by_ft = {
+        python = { "ruff_organize_imports", "ruff_fix" },
+        rust = { "rustfmt" },
+        go = { "goimports", "gofmt" },
+        javascript = { "prettier" },
+      },
+      format_on_save = {
+        timeout_ms = 1500,
+        lsp_format = "fallback",
+      },
+    })
+  end,
 }
--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -31,34 +31,6 @@ return {
       -- 補完の設定
       vim.cmd([[set completeopt+=menuone,noselect,popup]])
 
-      vim.api.nvim_create_autocmd("LspAttach", {
-        group = vim.api.nvim_create_augroup("lsp", { clear = true }),
-        callback = function(args)
-          vim.api.nvim_create_autocmd("BufWritePre", {
-            buffer = args.buf,
-            callback = function()
-              -- フォーマット可能なLSPサーバーが存在するか確認
-              local clients = vim.lsp.get_clients({ bufnr = args.buf })
-              for _, client in ipairs(clients) do
-                if client.supports_method("textDocument/formatting") then
-                  vim.lsp.buf.format { async = false, id = client.id }
-                  break
-                end
-              end
-            end,
-          })
-        end,
-      })
-
-      -- LSPサーバーの設定
-      vim.api.nvim_create_autocmd('LspAttach', {
-        callback = function(ev)
-          local client = vim.lsp.get_client_by_id(ev.data.client_id)
-          if client:supports_method('textDocument/completion') then
-            vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
-          end
-        end,
-      })
       -- LSPサーバーの有効化
       vim.lsp.enable(lsp_servers)
     end,
