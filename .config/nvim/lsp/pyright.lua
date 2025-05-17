c3f4c0beaf630895f7078bd5638f4d7be749d116
uuta <nightrage.metal66@gmail.com>
2025-05-17 18:50:32 +0900

    Update Pyright and Ruff LSP configurations

--- .config/nvim/lsp/pyright.lua
+++ .config/nvim/lsp/pyright.lua
@@ -1,16 +1,46 @@
+local new_capability = {
+  -- this will remove some of the diagnostics that duplicates those from ruff, idea taken and adapted from
+  -- here: https://github.com/astral-sh/ruff-lsp/issues/384#issuecomment-1989619482
+  textDocument = {
+    publishDiagnostics = {
+      tagSupport = {
+        valueSet = { 2 },
+      },
+    },
+    hover = {
+      contentFormat = { "plaintext" },
+      dynamicRegistration = true,
+    },
+  },
+}
+
 return {
+  cmd = { "pyright-langserver", "--stdio" },
   settings = {
     pyright = {
-      disableLanguageServices = false,
-      disableOrganizeImports = false,
-      disableTaggedHints = true
+      -- disable import sorting and use Ruff for this
+      disableOrganizeImports = true,
+      disableTaggedHints = false,
     },
     python = {
       analysis = {
-        autoImportCompletions = true,
         autoSearchPaths = true,
+        diagnosticMode = "workspace",
+        typeCheckingMode = "standard",
         useLibraryCodeForTypes = true,
+        -- we can this setting below to redefine some diagnostics
+        diagnosticSeverityOverrides = {
+          deprecateTypingAliases = false,
+        },
+        -- inlay hint settings are provided by pylance?
+        inlayHints = {
+          callArgumentNames = "partial",
+          functionReturnTypes = true,
+          pytestParameters = true,
+          variableTypes = true,
+        },
       },
-    }
+    },
   },
+  capabilities = new_capability,
 }
--- .config/nvim/lsp/ruff.lua
+++ .config/nvim/lsp/ruff.lua
@@ -1,7 +1,15 @@
+-- return {
+--   -- pyproject.tomlの設定が自動的に読み込まれる
+--   settings = {
+--     organizeImports = true,
+--     fixAll = true
+--   }
+-- }
 return {
-  -- pyproject.tomlの設定が自動的に読み込まれる
-  settings = {
-    organizeImports = true,
-    fixAll = true
-  }
+  init_options = {
+    -- the settings can be found here: https://docs.astral.sh/ruff/editors/settings/
+    settings = {
+      organizeImports = true,
+    },
+  },
 }
