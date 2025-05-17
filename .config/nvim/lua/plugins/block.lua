fcc707ae627719412215f5f464609756de62b4bf
uuta <nightrage.metal66@gmail.com>
2025-05-17 17:48:39 +0900

    Remove deprecated plugins and configurations

--- .config/nvim/lua/plugins/block.lua
+++
@@ -1,5 +0,0 @@
--- return {
---     "HampusHauffman/block.nvim",
---     config = function() require("block").setup({}) end
--- }
-return {}
--- .config/nvim/lua/plugins/cellular-automaton.lua
+++
@@ -1,4 +0,0 @@
--- https://github.com/Eandrju/cellular-automaton.nvim
--- :CellularAutomaton make_it_rain
--- :CellularAutomaton game_of_life
-return {'eandrju/cellular-automaton.nvim'}
