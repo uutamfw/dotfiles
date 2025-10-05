4713f99fe31e8b71e37d2d6f3f273d7464cb3782
Yuta Aoki <yutaaoki@Yutas-MacBook-Pro.local>
2025-10-05 14:34:24 +0900

    Remove venv-selector configuration

--- .config/nvim/lua/plugins/venv-selector.lua
+++ .config/nvim/lua/plugins/venv-selector.lua
@@ -1,22 +1,23 @@
-return {
-    "linux-cultist/venv-selector.nvim",
-    dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python" },
-    config = function()
-        require("venv-selector").setup({
-            -- Your options go here
-            -- auto_refresh = false,
-            -- search from workspace root
-            -- search = false,
-            -- search_workspace = true,
-            name = { "venv", ".venv" },
-        })
-    end,
-    event = "VeryLazy", -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
-    keys = {
-        -- Keymap to open VenvSelector to pick a venv.
-        { "<leader>vs", "<cmd>VenvSelect<cr>" },
-        -- Keymap to retrieve the venv from a cache (the one previously used for the same project directory).
-        { "<leader>vc", "<cmd>VenvSelectCached<cr>" },
-    },
-    version = "*",
-}
+return {}
+-- return {
+--     "linux-cultist/venv-selector.nvim",
+--     dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python" },
+--     config = function()
+--         require("venv-selector").setup({
+--             -- Your options go here
+--             -- auto_refresh = false,
+--             -- search from workspace root
+--             -- search = false,
+--             -- search_workspace = true,
+--             name = { "venv", ".venv" },
+--         })
+--     end,
+--     event = "VeryLazy", -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
+--     keys = {
+--         -- Keymap to open VenvSelector to pick a venv.
+--         { "<leader>vs", "<cmd>VenvSelect<cr>" },
+--         -- Keymap to retrieve the venv from a cache (the one previously used for the same project directory).
+--         { "<leader>vc", "<cmd>VenvSelectCached<cr>" },
+--     },
+--     version = "*",
+-- }
