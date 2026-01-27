cecb21a5c2c06a5a082ac8fdc20dd7c681d42f0b
uuta <nightrage.metal66@gmail.com>
2026-01-27 13:04:15 +0900

    Switch branch to main for nvim-treesitter

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,17 +1,19 @@
+-- INFO: switch branch to main
+-- @see: https://blog.atusy.net/2025/08/10/nvim-treesitter-main-branch/
 return {
-    "nvim-treesitter/nvim-treesitter",
-    -- Settings for comment out
-    -- https://github.com/JoosepAlviste/nvim-ts-context-commentstring/wiki/Integrations#plugins-with-a-pre-comment-hook
-    dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" },
-    config = function()
-        require("ts_context_commentstring").setup({
-            highlight = {
-                enable = true, -- syntax highlightを有効にする
-            },
-            ensure_installed = "all", -- :TSInstall allと同じ
-            ignore_install = { "phpdoc" },
-            -- ensure_installed = 'maintained' とすることもできる
-            context_commentstring = { enable = true, enable_autocmd = false },
-        })
-    end,
+  "nvim-treesitter/nvim-treesitter",
+  -- Settings for comment out
+  -- https://github.com/JoosepAlviste/nvim-ts-context-commentstring/wiki/Integrations#plugins-with-a-pre-comment-hook
+  dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" },
+  config = function()
+    require("ts_context_commentstring").setup({
+      highlight = {
+        enable = true,                -- syntax highlightを有効にする
+      },
+      ensure_installed = "all",       -- :TSInstall allと同じ
+      ignore_install = { "phpdoc" },
+      -- ensure_installed = 'maintained' とすることもできる
+      context_commentstring = { enable = true, enable_autocmd = false },
+    })
+  end,
 }
