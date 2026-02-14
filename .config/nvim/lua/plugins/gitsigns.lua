437c30bfa481bd023e2271644961975d62c197bc
uuta <nightrage.metal66@gmail.com>
2026-02-14 19:40:44 +0900

    Update gitsigns configuration settings

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -1,4 +1,10 @@
 return {
   'lewis6991/gitsigns.nvim',
-  config = function() require('gitsigns').setup() end,
+  config = function()
+    require('gitsigns').setup({
+      numhl     = true,           -- Color the line number when there is a git change
+      linehl    = false,          -- Color the whole line when there is a git change.
+      word_diff = true            -- Highlight the word that has changed when there is a git change.
+    })
+  end,
 }
